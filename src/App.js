9e99a139dbcc4f7731c7b0a5b59da79590fa3ac3
Tony W <bobacrypto2@gmail.com>
2023-05-02 00:29:46 -0700

    Create basic boilerplate files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React { useState, useEffect } from "react";
+import CardContainer from "./components/CardContainer";
+import NavBar from "./components/Navbar";
 
 const App = () => {
-  
+
 }
 
 export default App;
