9e6b0deee29c131720c45a33e0cef611265271c8
Tony W <bobacrypto2@gmail.com>
2023-05-06 01:11:33 -0700

    Was able to add an onClick randomizer and state change to tell our App one of the cards have been clicked before

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React, { useState, useEffect } from "react";
-import CardContainer from "./components/CardContainer";
+import Cards from "./components/Cards";
 import NavBar from "./components/Navbar";
 import './styles/App.css'
 import './styles/CardContainer.css'
 import './styles/Cards.css'
 import './styles/NavBar.css'
+import { tsConstructSignatureDeclaration } from "@babel/types";
 
 const App = () => {
+  const [cards, setCards] = useState({
+      sage: {
+        clicked: false
+      },
+      reyna: {
+        clicked: false
+      },
+      viper: {
+        clicked: false
+      },
+      neon: {
+        clicked: false
+      },
+      kj: {
+        clicked: false
+      },
+      fade: {
+        clicked: false
+      },
+      astra: {
+        clicked: false
+      },
+      raze: {
+        clicked: false
+      },
+      jett: {
+        clicked: false
+      },
+      skye: {
+        clicked: false
+      },
+  })
+
+  const [score, setScore] = useState(0);
   
   const characterImages = {
     sage: require('./images/sage.jpg'),
@@ -21,11 +56,45 @@ const App = () => {
     skye: require('./images/skye.webp')
   }
 
+  function randomizeCards(cards) {
+    for (let i = 0; i < 27; i++) {
+        let num1 = Math.floor(Math.random() * cards.length);
+        let num2 = Math.floor(Math.random() * cards.length);
+
+        if (num1 === num2) {
+            continue;
+        }
+
+        let card1 = cards[num1];
+        let card2 = cards[num2];
+
+        cards[num2] = card1;
+        cards[num1] = card2;
+    }
+
+    return cards;
+  }
+
+  // useEffect(() => {
+  //   Test on cards state change whether or not it was a
+  //   correct move ( click ) and depending on the answer we will
+  //   either increment state score or
+  //   tear it down and reset all of cards states back to how it was originally
+  // }, [cards])
+
+  function handleClick(card) {
+    setCards(allCards => ({
+      ...allCards,
+      [card]: {
+        clicked: true
+      }
+    }))
+  }
 
   return (
     <>
       <NavBar />
-      <CardContainer images={characterImages}/>
+      <Cards images={characterImages} randomize={randomizeCards} clickEvent={handleClick}/>
     </>
   )
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,33 +7,20 @@ const Cards = (props) => {
         const cardItems = [];
 
         for (let img in props.images) {
-            cardItems.push(<img className={`card ${img}`} key={`${uniqid()}`} alt="Character Card of Valorant Agent" src={props.images[img]}></img>)
+            cardItems.push(<img className={`card ${img}`} 
+            key={`${uniqid()}`} 
+            alt="Character Card of Valorant Agent" 
+            src={props.images[img]}
+            onClick={() => {props.clickEvent(img)}}></img>)
         }
         
-        return randomizeCards(cardItems);
-    }
-
-    function randomizeCards(cards) {
-        for (let i = 0; i < 27; i++) {
-            let num1 = Math.floor(Math.random() * cards.length);
-            let num2 = Math.floor(Math.random() * cards.length);
-
-            if (num1 === num2) {
-                continue;
-            }
-
-            let card1 = cards[num1];
-            let card2 = cards[num2];
-
-            cards[num2] = card1;
-            cards[num1] = card2;
-        }
-
-        return cards;
+        return props.randomize(cardItems);
     }
 
     return (
-        createCards()
+        <div id="card-container">
+            {createCards()}
+        </div>
     )
 }
 
