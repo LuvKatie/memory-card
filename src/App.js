276b0546200efcb2ee23c90857a7c450f635c806
Tony W <bobacrypto2@gmail.com>
2023-05-12 03:38:45 -0700

    Add reset button to play again | Add result message for defeat or victory

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Cards from "./components/Cards";
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -7,6 +7,7 @@ import './styles/CardContainer.css'
 import './styles/Cards.css'
 import './styles/NavBar.css'
 import './styles/Footer.css'
+import './styles/ResultMessage.css'
 
 const App = () => {
  
@@ -45,7 +46,7 @@ const App = () => {
 
   const [cards, setCards] = useState(initialCards)
 
-  const [score, setScore] = useState(0);
+  const [score, setScore] = useState(9);
   
   const characterImages = {
     sage: require('./images/sage.jpg'),
@@ -97,22 +98,40 @@ const App = () => {
     const leftBG = document.getElementById('left-bg');
     const rightBG = document.getElementById('right-bg');
     const bgImg = document.getElementById('bg-img');
+
     const cardContainer = document.getElementById('card-container');
     const footer = document.getElementById('footer');
+    const header = document.querySelector('header');
+
+    const playAgain = document.getElementById('play-again');
+    const resultContainer = document.getElementById('resultMessage')
+    const resultMessage = document.getElementById('result')
 
     const victoryGif = 'https://64.media.tumblr.com/973b2c5280b4b5179bb09bc28c9b97d5/29c64f10a3375b77-94/s640x960/352b9bcc1cb235c175790eaa6057abc0ef13ffeb.gifv';
     const defeatGif = 'https://64.media.tumblr.com/ebf2748f2064107a0d9dcec26ad4fe9a/9e1fd39c604fff15-5f/s640x960/000eac0c83086730fb579a08074ffb6513c96f80.gifv';;
     
     if (result === 'correct') {
       bgImg.setAttribute('style', `background-image: url(${victoryGif})`)
+      resultMessage.textContent = 'Victory'
+      resultMessage.classList.add('victory')
     } else if (result === 'incorrect') {
       bgImg.setAttribute('style', `background-image: url(${defeatGif})`)
+      resultMessage.classList.add('defeat')
+      resultMessage.textContent = 'Defeat'
     }
 
-    cardContainer.classList.add('hide-container');
-    footer.classList.add('hide-footer');
-    leftBG.classList.add('left-hover');
-    rightBG.classList.add('right-hover');
+    setTimeout(() => {
+      playAgain.classList.add('reveal-results');
+      resultContainer.classList.add('reveal-results');
+      resultMessage.classList.add('reveal-results');
+    }, 3000)
+
+    cardContainer.classList.add('hide-ui');
+    footer.classList.add('hide-ui');
+    header.classList.add('hide-ui');
+
+    leftBG.classList.add('left-reveal');
+    rightBG.classList.add('right-reveal');
   }
 
   function handleOverlays(card, result) {
@@ -125,6 +144,7 @@ const App = () => {
       } else if (overlay.className.includes(`${card}`) && result === 'incorrect') {
         overlay.classList.toggle('hideToggle');
         overlay.classList.add('incorrect');
+        // Display losing screen
         setTimeout(() => {
           resultScreen(result);
         }, 1200)
@@ -133,6 +153,7 @@ const App = () => {
         overlay.classList.add('not-selected');
       }
 
+      // Display winning screen
       if (score === 9) {
         setTimeout(() => {
           resultScreen(result);
@@ -152,7 +173,7 @@ const App = () => {
         }))
   
         setScore(score + 1)
-      }, 1500)
+      }, 1350)
       return;
     }
 
@@ -166,9 +187,51 @@ const App = () => {
 
       setScore(score + 1)
     } else {
-      setCards(initialCards)
-      setScore(0)
+      resetGame();
     }
+  }
+
+  function resetGame() {
+    const cardContainer = document.getElementById('card-container');
+    const footer = document.getElementById('footer');
+    const header = document.querySelector('header');
+
+    const playAgain = document.getElementById('play-again');
+    const resultContainer = document.getElementById('resultMessage')
+    const resultMessage = document.getElementById('result')
+
+    const leftBG = document.getElementById('left-bg');
+    const rightBG = document.getElementById('right-bg');
+    const bgImg = document.getElementById('bg-img');
+
+    leftBG.classList.remove('left-reveal');
+    rightBG.classList.remove('right-reveal');
+    leftBG.classList.add('left-hide');
+    rightBG.classList.add('right-hide');
+    
+    header.classList.remove('hide-ui')
+    cardContainer.classList.remove('hide-ui')
+    footer.classList.remove('hide-ui')
+
+    playAgain.classList.remove('reveal-results')
+    resultContainer.classList.remove('reveal-results')
+    resultMessage.classList = '';
+    
+    header.classList.add('reveal-component')
+    cardContainer.classList.add('reveal-component')
+    footer.classList.add('reveal-component')
+
+    setTimeout(() => {
+      leftBG.classList.remove('left-hide');
+      rightBG.classList.remove('right-hide');
+      header.classList.remove('reveal-component')
+      cardContainer.classList.remove('reveal-component')
+      footer.classList.remove('reveal-component')
+      bgImg.setAttribute('style', `background-image: ''`)
+    }, 2000)
+
+    setCards(initialCards)
+    setScore(0)
   }
 
   return (
@@ -180,6 +243,10 @@ const App = () => {
       <NavBar score={score}/>
       <Cards images={characterImages} randomize={randomizeCards} clickEvent={handleClick}/>
       <Footer />
+      <div id="resultMessage">
+        <span id="result"></span>
+        <button id="play-again" onClick={resetGame}>Play Again</button>
+      </div>
     </>
   )
 }
